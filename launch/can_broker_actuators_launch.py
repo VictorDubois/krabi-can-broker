6cbca8b85f45b2b35a572d91f75e6aace89fd180
Victor Dubois <victor.dubois@navya.tech>
2025-03-13 23:02:54 +0100

    add launch files

---
+++ launch/can_broker_actuators_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import launch_ros.actions
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            #prefix=['gdbserver localhost:3000'],
+            package='krabi_can_broker',
+            namespace='krabi_ns',
+            executable='actuators_broker',
+            name='actuators_broker'
+        )
+    ])
