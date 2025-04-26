8705a4e9b20dc8b3569222a4ba1877a499c6d002
Victor Dubois <victor.dubois@navya.tech>
2025-04-26 22:22:31 +0200

    add digital inputs + start adding AX12

--- include/actuators_broker.hpp
+++ include/actuators_broker.hpp
@@ -2,19 +2,22 @@
 
 #include "generic_can_broker.hpp"
 #include <krabi_msgs/msg/actuators2025.hpp>
+#include <krabi_msgs/msg/ax12_info.hpp>
 #include <krabi_msgs/msg/infos_stepper.hpp>
 #include <sensor_msgs/msg/battery_state.hpp>
+#include <std_msgs/msg/byte.hpp>
 #include <std_msgs/msg/float32.hpp>
 
-
 // Node class
-class CanActuatorBroker: public GenericCanBroker
+class CanActuatorBroker : public GenericCanBroker
 {
 public:
     CanActuatorBroker();
 
-    ~CanActuatorBroker() {    
-        if (can_socket_ != -1) {
+    ~CanActuatorBroker()
+    {
+        if (can_socket_ != -1)
+        {
             close(can_socket_);
         }
     };
@@ -23,15 +26,19 @@ private:
     rclcpp::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr battery_pub_;
     rclcpp::Publisher<krabi_msgs::msg::InfosStepper>::SharedPtr stepper_info_pub_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr vacuum_pub_;
-    
+    rclcpp::Publisher<krabi_msgs::msg::AX12Info>::SharedPtr AX12_pub_1;
+    rclcpp::Publisher<krabi_msgs::msg::AX12Info>::SharedPtr AX12_pub_2;
+    rclcpp::Publisher<krabi_msgs::msg::AX12Info>::SharedPtr AX12_pub_3;
+    rclcpp::Publisher<krabi_msgs::msg::AX12Info>::SharedPtr AX12_pub_4;
+    rclcpp::Publisher<std_msgs::msg::Byte>::SharedPtr digitalReads_pub;
     // Servo callback
-    void servoCallback(const krabi_msgs::msg::Actuators2025::SharedPtr msg) ;
+    void servoCallback(const krabi_msgs::msg::Actuators2025::SharedPtr msg);
 
     void receive_can_messages();
-    void publish_analog_sensors(const int16_t &battery_voltage_mV) ;
-    void publish_vacuum(const int16_t &stepper_info) ;
+    void publish_analog_sensors(const int16_t& battery_voltage_mV);
+    void publish_vacuum(const int16_t& stepper_info);
+    void publish_digital_sensors(const uint8_t& a_digital_intputs);
 
-    void publish_stepper_info(const CAN::StepperInfo* stepper_info) ;
+    void publish_stepper_info(const CAN::StepperInfo* stepper_info);
     rclcpp::Subscription<krabi_msgs::msg::Actuators2025>::SharedPtr actuators2025_sub_;
-
 };
--- src/actuators_broker.cpp
+++ src/actuators_broker.cpp
@@ -1,38 +1,48 @@
 #include "actuators_broker.hpp"
 
-
-CanActuatorBroker::CanActuatorBroker() : GenericCanBroker()
+CanActuatorBroker::CanActuatorBroker()
+  : GenericCanBroker()
 {
     // Create subscribers for each message type
     actuators2025_sub_ = this->create_subscription<krabi_msgs::msg::Actuators2025>(
-        "actuators2025", 10, std::bind(&CanActuatorBroker::servoCallback, this, std::placeholders::_1));
+      "actuators2025",
+      10,
+      std::bind(&CanActuatorBroker::servoCallback, this, std::placeholders::_1));
 
-        battery_pub_ = this->create_publisher<sensor_msgs::msg::BatteryState>("actuators_battery", 10);
-        vacuum_pub_ = this->create_publisher<std_msgs::msg::Float32>("vacuum", 10);
-        stepper_info_pub_ = this->create_publisher<krabi_msgs::msg::InfosStepper>("stepper_info", 10);
-
+    battery_pub_ = this->create_publisher<sensor_msgs::msg::BatteryState>("actuators_battery", 10);
+    vacuum_pub_ = this->create_publisher<std_msgs::msg::Float32>("vacuum", 10);
+    stepper_info_pub_ = this->create_publisher<krabi_msgs::msg::InfosStepper>("stepper_info", 10);
+    AX12_pub_1 = this->create_publisher<krabi_msgs::msg::AX12Info>("ax12_1_info", 10);
+    AX12_pub_2 = this->create_publisher<krabi_msgs::msg::AX12Info>("ax12_2_info", 10);
+    AX12_pub_3 = this->create_publisher<krabi_msgs::msg::AX12Info>("ax12_3_info", 10);
+    AX12_pub_4 = this->create_publisher<krabi_msgs::msg::AX12Info>("ax12_4_info", 10);
+    digitalReads_pub = this->create_publisher<std_msgs::msg::Byte>("digitalRead", 10);
 
     // Start a separate thread to listen to CAN messages
     can_receiver_thread_ = std::thread(&CanActuatorBroker::receive_can_messages, this);
 
-
     /*cmd_vel_sub_ = this->create_subscription<CmdVel>(
-        "cmd_vel", 10, std::bind(&CanActuatorBroker::cmdVelCallback, this, std::placeholders::_1));*/
+        "cmd_vel", 10, std::bind(&CanActuatorBroker::cmdVelCallback, this,
+       std::placeholders::_1));*/
 
     // Additional subscribers for other message types...
 }
 
-void CanActuatorBroker::receive_can_messages() {
+void CanActuatorBroker::receive_can_messages()
+{
     struct can_frame frame;
-    while (rclcpp::ok()) {
+    while (rclcpp::ok())
+    {
         int nbytes = read(can_socket_, &frame, sizeof(struct can_frame));
 
-        if (nbytes < 0) {
+        if (nbytes < 0)
+        {
             RCLCPP_ERROR(this->get_logger(), "CAN read error");
             continue;
         }
 
-        if (frame.can_id == CAN::can_ids::STEPPER_INFO && frame.can_dlc == sizeof(CAN::StepperInfo)) {
+        if (frame.can_id == CAN::can_ids::STEPPER_INFO && frame.can_dlc == sizeof(CAN::StepperInfo))
+        {
             CAN::StepperInfo stepper_info;
             stepper_info.distance_to_go = frame.data[1] | (frame.data[0] << 8);
             stepper_info.homing_sequences_done = frame.data[2];
@@ -40,7 +50,9 @@ void CanActuatorBroker::receive_can_messages() {
             publish_stepper_info(&stepper_info);
         }
 
-        if (frame.can_id == CAN::can_ids::ANALOG_SENSORS && frame.can_dlc == sizeof(CAN::AnalogSensors)) {
+        if (frame.can_id == CAN::can_ids::ANALOG_SENSORS
+            && frame.can_dlc == sizeof(CAN::AnalogSensors))
+        {
             CAN::AnalogSensors analogSensors;
             analogSensors.battery_mV = frame.data[1] | (frame.data[0] << 8);
             analogSensors.vacuum_1 = frame.data[3] | (frame.data[2] << 8);
@@ -48,11 +60,27 @@ void CanActuatorBroker::receive_can_messages() {
             publish_analog_sensors(analogSensors.battery_mV);
             publish_vacuum(analogSensors.battery_mV);
         }
+
+        if (frame.can_id == CAN::can_ids::DIGITAL_OUTPUTS
+            && frame.can_dlc == sizeof(CAN::DigitalOutputs))
+        {
+            publish_digital_sensors(frame.data[0]);
+        }
+
+        /*if (frame.can_id <= CAN::can_ids::AX12_R1 && frame.can_id <= CAN::can_ids::AX12_R6
+            && frame.can_dlc == sizeof(CAN::AX12Read))
+        {
+            CAN::AX12Read ax12_read;
+            ax12_read.current_position =
+
+            // publish_digital_sensors(frame.data[0]);
+        }*/
     }
 }
 
 // Servo callback
-void CanActuatorBroker::servoCallback(const krabi_msgs::msg::Actuators2025::SharedPtr msg) {
+void CanActuatorBroker::servoCallback(const krabi_msgs::msg::Actuators2025::SharedPtr msg)
+{
     struct can_frame frame;
     frame.can_id = CAN::can_ids::SERVO_1;
     frame.can_dlc = sizeof(CAN::ServoMessage);
@@ -78,15 +106,14 @@ void CanActuatorBroker::servoCallback(const krabi_msgs::msg::Actuators2025::Shar
     frame.data[7] = msg->servo_8.speed;
     send_can_frame(frame);
 
-
     frame.can_id = CAN::can_ids::STEPPER_CMD;
     frame.can_dlc = sizeof(CAN::Stepper);
     frame.data[0] = msg->stepper_1.speed >> 8;
-    frame.data[1] = msg->stepper_1.speed%256;
+    frame.data[1] = msg->stepper_1.speed % 256;
     frame.data[2] = msg->stepper_1.accel >> 8;
-    frame.data[3] = msg->stepper_1.accel%256;
+    frame.data[3] = msg->stepper_1.accel % 256;
     frame.data[4] = msg->stepper_1.position >> 8;
-    frame.data[5] = msg->stepper_1.position%256;
+    frame.data[5] = msg->stepper_1.position % 256;
     frame.data[6] = msg->stepper_1.current;
     frame.data[7] = msg->stepper_1.mode;
     send_can_frame(frame);
@@ -101,7 +128,28 @@ void CanActuatorBroker::servoCallback(const krabi_msgs::msg::Actuators2025::Shar
     send_can_frame(frame);
 }
 
-void CanActuatorBroker::publish_analog_sensors(const int16_t &battery_voltage_mV) {
+void CanActuatorBroker::publish_digital_sensors(const uint8_t& a_digital_intputs)
+{
+    // Convert the AnalogSensors struct to a ROS2 message (e.g., BatteryState for illustration)
+    auto msg = std_msgs::msg::Byte();
+    msg.data = a_digital_intputs;
+
+    RCLCPP_INFO(this->get_logger(),
+                "Publishing a_digital_intputs: %d %d %d %d %d %d %d %d",
+                a_digital_intputs << 0 & 0x1,
+                a_digital_intputs << 1 & 0x1,
+                a_digital_intputs << 2 & 0x1,
+                a_digital_intputs << 3 & 0x1,
+                a_digital_intputs << 4 & 0x1,
+                a_digital_intputs << 5 & 0x1,
+                a_digital_intputs << 6 & 0x1,
+                a_digital_intputs << 7 & 0x1);
+
+    digitalReads_pub->publish(msg);
+}
+
+void CanActuatorBroker::publish_analog_sensors(const int16_t& battery_voltage_mV)
+{
     // Convert the AnalogSensors struct to a ROS2 message (e.g., BatteryState for illustration)
     auto msg = sensor_msgs::msg::BatteryState();
     msg.voltage = battery_voltage_mV / 1000.0; // Convert mV to V
@@ -112,30 +160,34 @@ void CanActuatorBroker::publish_analog_sensors(const int16_t &battery_voltage_mV
     battery_pub_->publish(msg);
 }
 
-void CanActuatorBroker::publish_vacuum(const int16_t &vacuum) {
+void CanActuatorBroker::publish_vacuum(const int16_t& vacuum)
+{
     auto msg = std_msgs::msg::Float32();
-    msg.data = vacuum/1000.0;
+    msg.data = vacuum / 1000.0;
 
     RCLCPP_INFO(this->get_logger(), "Publishing Vacuum: %d", vacuum);
 
     vacuum_pub_->publish(msg);
 }
 
-void CanActuatorBroker::publish_stepper_info(const CAN::StepperInfo* stepper_info) {
+void CanActuatorBroker::publish_stepper_info(const CAN::StepperInfo* stepper_info)
+{
     // Convert the AnalogSensors struct to a ROS2 message (e.g., BatteryState for illustration)
     auto msg = krabi_msgs::msg::InfosStepper();
     msg.distance_to_go = stepper_info->distance_to_go;
     msg.homing_sequences_done = stepper_info->homing_sequences_done;
     msg.homing_switch_on = stepper_info->homing_switches_on;
 
-    RCLCPP_INFO(this->get_logger(), "Publishing StepperInfo: distance_to_go=%d, homing_sequences_done = %d", stepper_info->distance_to_go, stepper_info->homing_sequences_done);
+    RCLCPP_INFO(this->get_logger(),
+                "Publishing StepperInfo: distance_to_go=%d, homing_sequences_done = %d",
+                stepper_info->distance_to_go,
+                stepper_info->homing_sequences_done);
 
     stepper_info_pub_->publish(msg);
 }
 
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     rclcpp::init(argc, argv);
     auto node = std::make_shared<CanActuatorBroker>();
     rclcpp::spin(node);
